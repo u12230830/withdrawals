8446c623a4a9e20da78efd4652a4ae702b4d8bd9
Fako Peleha <Fako.Peleha@standardbank.co.za>
2023-06-11 18:39:33 +0200

    Change init log line

--- withdrawal-engine-web/src/main/java/za/co/momentum/Main.java
+++ withdrawal-engine-web/src/main/java/za/co/momentum/Main.java
@@ -2,6 +2,6 @@ package za.co.momentum;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Withdrawal Service Engine::start up");
     }
 }
